b7588f7af69e199465df179b7f4e905631d06b7d
Lacey Umamoto <lcumamoto@gmail.com>
2021-02-20 12:34:43 -0800

    added sidebar nav

--- tennis.py
+++ tennis.py
@@ -4,40 +4,52 @@ import streamlit as st
 def parse(date):
     return "{}-{}-{}".format(date[:4], date[4:6], date[6:])
 
-st.title("Women's Tennis Association") 
-st.info("Displays a chosen player's matches and number of wins and losses.")
+# INDIVIDUAL PLAYER STATS
+def indiv_stats():
+    st.title("Women's Tennis Association") 
+    st.subheader('Individual Stats')
+    # st.info("Displays a chosen player's matches and number of wins and losses.")
 
-players = pd.read_csv('./data/players.csv', encoding="ISO-8859-1")
-matches = pd.read_csv('./data/matches.csv', encoding="ISO-8859-1")
+    players = pd.read_csv('./data/players.csv', encoding="ISO-8859-1")
+    matches = pd.read_csv('./data/matches.csv', encoding="ISO-8859-1")
 
-players['full_name'] = players['first_name'] + ' ' + players['last_name']
+    players['full_name'] = players['first_name'] + ' ' + players['last_name']
 
-st.header('Select a player')
-player = st.selectbox('Player Name', players['full_name'])
-player_id = players.loc[players['full_name'] == player, 'player_id'].iloc[0]
-hand = players.loc[players['player_id'] == player_id, 'hand'].iloc[0]
-dob = players.loc[players['player_id'] == player_id, 'birth_date'].iloc[0]
-dob = parse(str(int(dob)))
-country = players.loc[players['player_id'] == player_id, 'country_code'].iloc[0]
+    st.header('Select a player')
+    player = st.selectbox('Player Name', players['full_name'])
+    player_id = players.loc[players['full_name'] == player, 'player_id'].iloc[0]
+    hand = players.loc[players['player_id'] == player_id, 'hand'].iloc[0]
+    dob = players.loc[players['player_id'] == player_id, 'birth_date'].iloc[0]
+    dob = parse(str(int(dob)))
+    country = players.loc[players['player_id'] == player_id, 'country_code'].iloc[0]
 
-st.header(player)
-st.markdown(f'**Country: ** {country}')
-st.markdown(f'**DOB: ** {dob}')
-st.markdown(f'**Hand: ** {hand}')
+    st.header(player)
+    st.markdown(f'**Country: ** {country}')
+    st.markdown(f'**DOB: ** {dob}')
+    st.markdown(f'**Hand: ** {hand}')
 
-player_matches_losses = matches.loc[matches['loser_id'] == player_id]
-player_matches_wins = matches.loc[matches['winner_id'] == player_id]
+    player_matches_losses = matches.loc[matches['loser_id'] == player_id]
+    player_matches_wins = matches.loc[matches['winner_id'] == player_id]
 
-st.subheader('Matches Lost')
-st.write(len(player_matches_losses), ' matches lost.')
-# st.markdown(f'**{len(player_matches_losses)}** matches lost.')
-# st.table(player_matches_losses)
+    st.subheader('Matches Lost')
+    st.write(len(player_matches_losses), ' matches lost.')
 
-st.subheader('Matches Won')
-st.write(len(player_matches_wins), ' matches won.')
-# st.table(player_matches_wins)
+    st.subheader('Matches Won')
+    st.write(len(player_matches_wins), ' matches won.')
 
-st.subheader('All Matches')
-player_matches_all = pd.concat([player_matches_losses, player_matches_wins])
-st.write(len(player_matches_all), ' matches total.')
-st.table(player_matches_all)
+    st.subheader('All Matches')
+    player_matches_all = pd.concat([player_matches_losses, player_matches_wins])
+    st.write(len(player_matches_all), ' matches total.')
+    st.table(player_matches_all)
+
+def main():
+    nav = ['Home', 'Individual', 'Overall']
+    choice = st.sidebar.selectbox('Navigation', nav)
+    if choice == 'Home':
+        pass
+    elif choice == 'Individual':
+        indiv_stats()
+    elif choice == 'Overall':
+        pass
+
+main() 
