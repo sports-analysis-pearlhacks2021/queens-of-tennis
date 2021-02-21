6e545b2d637c70d551c1300e1e3dfb04e75294d4
Lacey Umamoto <lcumamoto@gmail.com>
2021-02-20 19:42:40 -0800

    added win/loss ratio pie chart

--- tennis.py
+++ tennis.py
@@ -1,10 +1,55 @@
 import pandas as pd 
 import streamlit as st 
-import plotly.graph_objects as go
-from plotly import graph_objs as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import plotly.graph_objects as go
+# from plotly import graph_objs as go
+
+from streamlit_echarts import st_echarts
+
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
 
 def parse(date):
     return "{}-{}-{}".format(date[:4], date[4:6], date[6:])
+
+def display_winlossratio(num_wins, num_losses):
+    option = {
+        'title' : {
+            'text': 'Win/Loss Ratio',
+            #'subtext': '',
+            'x':'center'
+        },
+        'tooltip' : {
+            'trigger': 'item',
+            'formatter': "{b} ({d}%)"
+        },
+        'legend': {
+            'orient': 'vertical',
+            'left': 'left',
+            'data': ['Wins', 'Losses']
+        },
+        'series' : [{
+            'name': 'Wins/Losses',
+            'type': 'pie',
+            'radius' : '55%',
+            'center': ['50%', '60%'],
+            'data':[
+                {'value':num_wins, 'name':str(num_wins) + ' Wins'},
+                {'value':num_losses, 'name':str(num_losses) + ' Losses'},
+            ],
+            'itemStyle': {
+                'emphasis': {
+                'shadowBlur': 10,
+                'shadowOffsetX': 0,
+                'shadowColor': 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }]
+    }
+    st_echarts(options=option)
 
 # INDIVIDUAL PLAYER STATS
 def indiv_stats(): 
@@ -36,33 +81,44 @@ def indiv_stats():
     player_matches_losses = matches.loc[matches['loser_id'] == player_id]
     player_matches_wins = matches.loc[matches['winner_id'] == player_id]
 
-    st.subheader('Matches Lost')
-    st.write(len(player_matches_losses), ' matches lost.')
+    num_wins = len(player_matches_wins)
+    num_losses = len(player_matches_losses)
 
-    st.subheader('Matches Won')
-    st.write(len(player_matches_wins), ' matches won.')
+    # st.subheader('Matches Won')
+    # st.write(num_wins, ' matches won.')
+
+    # st.subheader('Matches Lost')
+    # st.write(num_losses, ' matches lost.')
+
+    display_winlossratio(num_wins, num_losses)
 
     st.subheader('All Matches')
-    # player_matches_all = pd.concat([player_matches_losses, player_matches_wins])
-    # st.write(len(player_matches_all), ' matches total.')
+    player_matches_all = pd.concat([player_matches_losses, player_matches_wins])
+    st.write(len(player_matches_all), ' matches total.')
     # st.table(player_matches_all)
 
-    st.subheader('Tourneys')
-    player_matches_tourneys = matches.loc[matches['winner_id'] == player_id, 'tourney_name']
-    st.table(player_matches_tourneys)
+    # st.subheader('Tourneys')
+    # player_matches_tourneys = matches.loc[matches['winner_id'] == player_id, 'tourney_name']
+    # st.table(player_matches_tourneys)
 
 def overall_stats():
     st.header('Overall Stats')
 
+def home():
+    st.header('Home')
+    st.text('Welcome to the homepage!')
+
 def main():
+    st.sidebar.title("Women's Tennis Association")
+
     nav = ['Individual', 'Overall', 'Home']
     choice = st.sidebar.selectbox('Navigation', nav)
-    st.title("Women's Tennis Association")
+    
     if choice == 'Individual':
         indiv_stats()
     elif choice == 'Overall':
-        pass
+        overall_stats()
     elif choice == 'Home':
-        pass
+        home()
 
 main() 
